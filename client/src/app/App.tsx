3a8e088a786cab23c682843eac6555caab467126
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2020-01-08 19:29:25 +0200

    Use redux saga.

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 
 import { IndexPage, PersonPage, NotFoundPage } from './pages'
-import { getPersons } from './services/personService'
 import { useSelector } from './ducks'
 
 export function App() {
@@ -11,22 +10,7 @@ export function App() {
   const { persons, isLoading, isError } = useSelector((state) => state.person)
 
   useEffect(() => {
-    async function fetchData() {
-      dispatch({ type: 'FETCH_PERSONS' })
-
-      const response = await getPersons()
-
-      if (response.success) {
-        dispatch({
-          type: 'FETCH_PERSONS_SUCCESS',
-          payload: { response: response.value }
-        })
-      } else {
-        dispatch({ type: 'FETCH_PERSONS_FAILURE' })
-      }
-    }
-
-    fetchData()
+    dispatch({ type: 'FETCH_PERSONS' })
   }, [dispatch])
 
   const handleRemovePerson = (uuid: string) => {
