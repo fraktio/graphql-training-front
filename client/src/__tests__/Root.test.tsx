aeb0e83451152e9477eecfd1367ae771978b9c77
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2020-01-08 07:55:20 +0200

    Added routing. Refactored components into their own folders.

--- client/src/__tests__/Root.test.tsx
+++ client/src/__tests__/Root.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-import { Root } from '../app/components/Root'
+import { Root } from '../app/Root'
 
 test('renders the main screen', async () => {
   const { getByText } = render(<Root />)
