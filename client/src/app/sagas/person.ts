bce2dcf02e005bce33ee397dda197ccfbf7794e5
Mikko Hirvonen <mikko.hirvonen@fraktio.fi>
2020-01-09 19:56:18 +0200

    Actually remove and add persons from the server.

--- client/src/app/sagas/person.ts
+++ client/src/app/sagas/person.ts
@@ -1,6 +1,7 @@
 import { call, put } from 'redux-saga/effects'
 
-import { getPersons } from '../services/personService'
+import { getPersons, removePerson, addPerson } from '../services/personService'
+import { RemovePersonAction, AddPersonAction } from '../ducks/person'
 
 export function* fetchPersons() {
   const response = yield call(getPersons)
@@ -12,5 +13,40 @@ export function* fetchPersons() {
     })
   } else {
     yield put({ type: 'FETCH_PERSONS_FAILURE' })
+  }
+}
+
+export function* removePersonRequest(action: RemovePersonAction) {
+  const { uuid } = action.payload
+
+  const response = yield call(removePerson, uuid)
+
+  if (response.success) {
+    yield put({
+      type: 'REMOVE_PERSON_SUCCESS',
+      payload: {
+        uuid
+      }
+    })
+  } else {
+    yield put({ type: 'REMOVE_PERSON_FAILURE' })
+  }
+}
+
+export function* addPersonRequest(action: AddPersonAction) {
+  const { firstName, lastName } = action.payload
+
+  const response = yield call(addPerson, { firstName, lastName })
+
+  if (response.success) {
+    yield put({
+      type: 'ADD_PERSON_SUCCESS',
+      payload: {
+        firstName,
+        lastName
+      }
+    })
+  } else {
+    yield put({ type: 'ADD_PERSON_FAILURE' })
   }
 }
